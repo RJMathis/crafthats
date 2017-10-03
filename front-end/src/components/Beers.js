3490707e78e6e5d657c230d8f3f9761c2155658d
Travis Reed <travisreed7@gmail.com>
2017-10-03 11:25:33 -0500

    Add simple tests for each component, Change class to className in Beers component

--- front-end/src/components/Beers.js
+++ front-end/src/components/Beers.js
@@ -3,34 +3,34 @@ import React, { Component } from 'react';
 export default class Beers extends Component {
   render() {
     return (
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4 container-thumbnail">
-              <div class="text-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 container-thumbnail">
+              <div className="text-center">
                 <a href="BronxRyePaleAle.html">
-                  <img class="img-thumbnail" src="https://s3.amazonaws.com/brewerydbapi/beer/wSybgO/upload_YO6evM-medium.png" alt="Bronx Rye Pale Ale" title="Bronx Rye Pale Ale"/>
-                  <div class="overlay">
-                    <div class="text">Bronx Rye Pale Ale</div>
+                  <img className="img-thumbnail" src="https://s3.amazonaws.com/brewerydbapi/beer/wSybgO/upload_YO6evM-medium.png" alt="Bronx Rye Pale Ale" title="Bronx Rye Pale Ale"/>
+                  <div className="overlay">
+                    <div className="text">Bronx Rye Pale Ale</div>
                   </div>
                 </a>
               </div>
             </div>
-            <div class="col-md-4 container-thumbnail">
-              <div class="text-center">
+            <div className="col-md-4 container-thumbnail">
+              <div className="text-center">
                 <a href="CrownLight.html">
-                  <img class="img-thumbnail" src="http://via.placeholder.com/200x200" alt="Crown Light"/>
-                  <div class="overlay">
-                    <div class="text">Crown Light</div>
+                  <img className="img-thumbnail" src="http://via.placeholder.com/200x200" alt="Crown Light"/>
+                  <div className="overlay">
+                    <div className="text">Crown Light</div>
                   </div>
                 </a>
               </div>
             </div>
-            <div class="col-md-4 container-thumbnail">
-              <div class="text-center">
+            <div className="col-md-4 container-thumbnail">
+              <div className="text-center">
                 <a href="BrooklynEastIndiaPaleAle.html">
-                  <img class="img-thumbnail" src="https://s3.amazonaws.com/brewerydbapi/beer/Wrh3tC/upload_otteRK-medium.png" alt="Brooklyn East India Pale Ale"/>
-                  <div class="overlay">
-                    <div class="text">Brooklyn East India Pale Ale</div>
+                  <img className="img-thumbnail" src="https://s3.amazonaws.com/brewerydbapi/beer/Wrh3tC/upload_otteRK-medium.png" alt="Brooklyn East India Pale Ale"/>
+                  <div className="overlay">
+                    <div className="text">Brooklyn East India Pale Ale</div>
                   </div>
                 </a>
               </div>
