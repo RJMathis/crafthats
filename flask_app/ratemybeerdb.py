b46717dd5917f19aa0cf0a4452037206ac03bf3c
Pablo Velasco <pulvel@gmail.com>
2017-10-25 14:50:34 -0500

    Updated models

--- flask_app/ratemybeerdb.py
+++ flask_app/ratemybeerdb.py
@@ -1,12 +1,16 @@
+
+import json
 import requests
+from pprint import pprint
+  
+i = "Bronx Rye Pale Ale"
+url = "https://api.ratebeer.com/v1/api/graphql/"
 
-url = "https://api.ratebeer.com/v1/api/graphql"
-
-payload = "{\"query\":\"query {beer(id:4444){name id}}\",\"variables\":\"{}\",\"operationName\":null}"
+payload = "{\"query\":\"query {\\n  beerSearch(query: \\\"%s\\\")\\n  {\\n    items\\n    {\\n      name\\n      id\\n    }\\n  }\\n  # beerReviews(beerId: 183510)\\n  # {\\n  #   items\\n  #   {\\n  #     score\\n  #     beer\\n  #     {\\n  #       name\\n  #     }\\n  #     comment\\n  #     createdAt\\n  #   }\\n  # }\\n}\",\"variables\":\"{}\"}" % (i)
 headers = {
     'content-type': "application/json",
     'accept': "application/json",
-    'x-api-key': "OgsnJG7Hev1GabHNfLmgGzTvM6HiCHiaiRASmme0"
+    'x-api-key': "OgsnJG7Hev1GabHNfLmgGzTvM6HiCHiaiRASmme0",
     }
 
 response = requests.request("POST", url, data=payload, headers=headers)
