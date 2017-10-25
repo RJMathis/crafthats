e5457950f7e3ff9dbc0ef0b91ae31d5c7e5bea39
Samuil Borisov <TexasGuardsman@Samuils-MacBook-Air.local>
2017-10-24 19:02:59 -0500

    new ORM update

--- flask/models.py
+++ flask/models.py
@@ -3,50 +3,50 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+BreweryStyleAssocTable = db.Table('BreweryStyleAssocTable', 
+    db.Column('style_id',db.Integer, db.ForeignKey('Style.id')),
+    db.Column('brewery_name',db.String(64), db.ForeignKey('Brewery.name'))
+    ) 
+
 class Beer (db.Model):
     __tablename__ = "Beer"
-    name = db.Column('name', db.String(64), primary_key = True)
-    brewery = db.Column('brewery', db.String(64), db.ForeignKey('Brewery.name'))
-    style_id = db.Column('style_id', db.Integer, db.ForeignKey('Style.id'))
-    organic = db.Column('organic', db.Boolean)
-    abv = db.Column('abv', db.Integer)
+    name = db.Column(db.String(64), primary_key = True)
+    organic = db.Column(db.Boolean)
+    abv = db.Column(db.Integer)
+    
+    brewery_name = db.Column(db.String(64), db.ForeignKey('Brewery.name'))
+    style_id = db.Column(db.Integer, db.ForeignKey('Style.id'))
 
-    style = db.relationship('Style', foreign_keys=style_id)
-    brewery = db.relationship('Brewery', foreign_keys=brewery)
-
-class Style (db.Model):
-    __tablename__ = "Style"
-    id = db.Column('id', db.Integer, primary_key = True, autoincrement=True)
-    description = db.Column('description',db.Text)
-    ibu = db.Column('ibu', db.String(64))
-    abv = db.Column('abv', db.Integer)
+    reviews = db.relationship("Review", backref='beer', lazy='dynamic')
 
 class Brewery (db.Model):
     __tablename__ = "Brewery"
-    name = db.Column('name', db.String(64), primary_key = True)
-    city = db.Column('city', db.String(64))
-    state = db.Column('state', db.String(64))
-    established = db.Column('established', db.Integer)
+    name = db.Column(db.String(64), primary_key = True)
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(64))
+    established = db.Column(db.Integer)
 
-    
+    beers = db.relationship("Beer", backref ="brewery", lazy='dynamic')
+    reviews = db.relationship("Review", backref="brewery", lazy='dynamic')
+
+    styles = db.relationship ('Style', secondary=BreweryStyleAssocTable, backref=db.backref('brewery',lazy='dynamic'))
+
+class Style (db.Model):
+    __tablename__ = "Style"
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    description = db.Column(db.Text)
+    ibu = db.Column(db.String(64))
+    abv = db.Column(db.Integer)
+
+    beers = db.relationship("Beer", backref="style", lazy='dynamic')
 
 class Review (db.Model):
     __tablename__ = "Review"
-    id = db.Column('id', db.Integer, primary_key = True, autoincrement=True)
-    brewery_name = db.Column('brewery_name', db.String(64), db.ForeignKey('Brewery.name')) 
-    beer_name = db.column('beer_name', db.String(64),db.ForeignKey('Beer.name'))
-    rating = db.Column('rating', db.Numeric(1,1))
-    flavor = db.Column('flavor', db.Numeric(1,1))
-    comment = db.Column('comment', db.Text)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
+    rating = db.Column(db.Numeric(1,1))
+    flavor = db.Column(db.Numeric(1,1))
+    comment = db.Column(db.Text)
+    beer_name = db.column(db.String(64),db.ForeignKey('Beer.name'))    
+    brewery_name = db.Column(db.String(64), db.ForeignKey('Brewery.name')) 
 
-    # beer = db.relationship('Beer', foreign_keys=beer_name)
-    # brewery = db.relationship('Brewery',foreign_keys=brewery_name)
-
-class Stylebreweryassoc (db.Model):
-    __tablename__ = "StyleBreweryAssoc"
-    id = db.Column('id', db.Integer, primary_key = True)
-    style_id = db.Column('style_id', db.Integer, db.ForeignKey('Style.id'))
-    brewery_name = db.Column('brewery_name', db.String(64), db.ForeignKey('Brewery.name'))
-
-    style = db.relationship('Style', foreign_keys=style_id)
-    brewery = db.relationship('Brewery', foreign_keys=brewery_name)
+ 
