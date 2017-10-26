e1ed8a2c2412099efc82dd816ee235c866368497
RJMathis <rmat10267@gmail.com>
2017-10-26 14:49:46 -0500

    currently dev_app working with models

--- flask_app/main.py
+++ flask_app/main.py
@@ -121,5 +121,5 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, config=None, debug=True)
 
