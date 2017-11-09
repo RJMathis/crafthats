d675938aa9f76fd0054ddd77f6151159c32121b9
RJMathis <rmat10267@gmail.com>
2017-11-09 17:08:38 -0600

    Updated api to deal with sorting

--- flask_app/views/views.py
+++ flask_app/views/views.py
@@ -9,7 +9,7 @@ def home():
 
 @app.errorhandler(500)
 def server_error(e):
-    logging.exception('An error occurred during a request.')
+    #logging.exception('An error occurred during a request.')
     return """
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
