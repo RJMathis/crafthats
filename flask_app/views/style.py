acf7639dc6afdab7b2867586817e0fd6194a460f
FixBayonets <samuil.borisov@utexas.edu>
2017-11-09 18:23:28 -0600

    filter style srm

--- flask_app/views/style.py
+++ flask_app/views/style.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify
 from main import app
 from models import db, Style
+import math
 
 
 #GET ALL STYLES
@@ -70,3 +71,31 @@ def getStyleInfo(style_id):
     except AttributeError:
         return "Server Error 500: Invalid style_id"
     return jsonify(s)
+
+@app.route('/styles/srm/<float:srm_val>', methods = ['GET'])
+def filterStyleBySRM(srm_val):
+    allStyles = []
+    lim = request.args.get('limit', 9)
+    off = request.args.get('offset',0)
+    srm_val = math.floor(srm_val)
+    mod = 1.0 - (srm_val % 1)
+    styles = Style.query.filter(Style.srm >= srm_val).filter(Style.srm < (srm_val + mod)).limit(lim).offset(off).all()
+    for style in styles:
+        s = {
+        'type' : "style",
+        'id' : style.id,
+        'name' : style.name,
+        'description' : style.description,
+        'ibu_min' : style.ibu_min,
+        'ibu_max' : style.ibu_max,
+        'abv_min' : style.abv_min,
+        'abv_max' : style.abv_max,
+        'srm'     : style.srm,
+        'beers' : [beer.serializeName for beer in style.beers],
+        'breweries':[brewery.serializeName for brewery in style.breweries]
+        }
+        allStyles.append(s)
+
+    return jsonify(allStyles)
+
+
