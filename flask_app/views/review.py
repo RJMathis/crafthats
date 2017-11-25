d0d4035fbc3ab78b76e1a61ce477a1c468d57e97
RJMathis <rmat10267@gmail.com>
2017-11-24 21:10:26 -0600

    added comments

--- flask_app/views/review.py
+++ flask_app/views/review.py
@@ -7,42 +7,54 @@ from werkzeug.contrib.cache import SimpleCache
 cache = SimpleCache()
 
 #GET ALL REVIEWS
+"""
+/reviews endpoint
+?params:
+    +beer_name - string
+    +rating - float
+    +offset - int, default 0
+    +limit - int, default 12
+    +order - int
+"""
 @app.route('/reviews', methods = ['GET'])
 def getReviews():
+    # reviews to return
     allReviews = []
-     
 
+    # get optional params
     rating = request.args.get('rating','None').encode('utf-8')
     beer_name = request.args.get('beer_name','None').encode('utf-8')
     order = request.args.get('sort_by','default').encode('utf-8')
-    lim = request.args.get('limit', '9').encode('utf-8')
+    lim = request.args.get('limit', '12').encode('utf-8')
     off = request.args.get('offset', '0').encode('utf-8')
+
+    #cast
     lim = int(lim)
     off = int(off)
+
+    #caching
     cachestr = str(rating)+beer_name+order+str(lim)+str(off)
     rv = cache.get(cachestr)
     if rv is not None:
         return rv
-    
+
+    #build up the query with filters and store resulting list in styles array
+    query = db.session.query(Review)
     if beer_name != 'None':
         beer = db.session.query(Beer).filter_by(name=beer_name).first()
         beer_name= beer.id
-    filtersDict ={
-        'rating':rating,
-        'beer_name':beer_name
-    }
-    query = db.session.query(Review)
-    for attr,value in filtersDict.iteritems():
-        if value != 'None':
-            query = query.filter(getattr(Review,attr)==value)
+        query = query.filter(Review.beer_name == beer_name)
+    if rating != 'None':
+        query = query.filter(Review.rating >= rating)
     
-    totalCount =query.count()
     if order == "asc":
         reviews = query.order_by(Review.rating).limit(lim).offset(off).all()
     elif order == "desc":
         reviews = query.order_by(Review.rating.desc()).limit(lim).offset(off).all()
     else:
         reviews = query.limit(lim).offset(off).all()
+
+    totalCount =query.count()
 
     for review in reviews:
         r = {
@@ -64,6 +76,10 @@ def getReviews():
     return response
 
 # GET REVIEW BY ID
+"""
+/reviews/<review_id> endpoint
+review_id - integer
+"""
 @app.route('/reviews/<review_id>', methods = ['GET'])
 def getReviewInfo(review_id):
     try:
--- flask_app/views/style.py
+++ flask_app/views/style.py
@@ -7,31 +7,47 @@ from werkzeug.contrib.cache import SimpleCache
 cache = SimpleCache()
 
 #GET ALL STYLES
+"""
+/styles endpoint
+?params:
+    +abv_min - int
+    +abv_max - int
+    +srm_min - int
+    +srm_max - int
+    +offset - int, default 0
+    +limit - int, default 12
+    +order - int
+"""
 @app.route('/styles',methods=['GET'])
 def getStyles():
+    # styles to return
     allStyles = []
+
+    # get optional params
     abv_min = request.args.get('abv_min','None').encode('utf-8')
     abv_max = request.args.get('abv_max','None').encode('utf-8')
     srm_min = request.args.get('srm_min','None').encode('utf-8')
-    srm_max = request.args.get('srm_max',"None").encode('utf-8')
+    srm_max = request.args.get('srm_max','None').encode('utf-8')
     order = request.args.get('order','default').encode('utf-8')
-    lim = request.args.get('limit', '9').encode('utf-8')
+    lim = request.args.get('limit', '12').encode('utf-8')
     off = request.args.get('offset', '0').encode('utf-8')
+
+    #cast
     lim = int(lim)
     off = int(off)
-    
+
+    #caching
     cachestr = abv_min+abv_max+srm_min+srm_max+order+str(lim)+str(off)
     rv = cache.get(cachestr)
     if rv is not None:
         return rv
 
+    #build up the query with filters and store resulting list in styles array
     query = db.session.query(Style)
     if abv_min != 'None' and abv_max != 'None':
-        query = query.filter(Style.abv_min<=(float(abv_min)+1.0)).filter(Style.abv_max<=abv_max) 
+        query = query.filter(Style.abv_min>=abv_min).filter(Style.abv_max<=abv_max)
     if srm_min != 'None' and srm_max != 'None':
         query = query.filter(Style.srm>=srm_min).filter(Style.srm<=srm_max)
-    
-
     if order == "asc":
         styles = query.order_by(Style.name).limit(lim).offset(off).all()
     elif order == "desc":
@@ -40,7 +56,6 @@ def getStyles():
         styles = query.limit(lim).offset(off).all()
 
     totalCount = query.count()
-
 
     for style in styles:
         s = {
@@ -64,7 +79,11 @@ def getStyles():
     cache.set(cachestr,response, timeout= 5*60)
     return response
 
-
+#GET STYLE BY STYLE_ID
+"""
+/styles/<style_id> endpoint
+style_id - integer
+"""
 @app.route('/styles/<style_id>', methods = ['GET'])
 def getStyleInfo(style_id):
     try:
