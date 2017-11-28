9c4273b987e87dfc4c7d5891d0f078c9fb1b3615
FixBayonets <samuil.borisov@utexas.edu>
2017-11-27 20:30:08 -0600

    travis coverage report add

--- flask_app/run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python brewery_unit_tests.py
--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-cd flask_app
-python brewery_unit_tests.py
+#cd flask_app
+#python brewery_unit_tests.py
+coverage run flask_app/brewery_unit_tests.py
+coverage report flask_app/views/*.py
